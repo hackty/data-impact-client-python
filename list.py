5214668a6ac10ec11ded282ed793dd9f0d6774dd
Evan <yjq877978471@gmail.com>
2018-09-20 13:46:36 +0800

    update

--- list.py
+++ list.py
@@ -6,15 +6,16 @@ import utils
 
 def run(args):
     real_path = "list.txt"
+    real_dirs = utils.get_dirs(args.path)
     with open(real_path, 'r') as f:
         dirs = f.readlines()
         for i in range(len(dirs)):
             dirs[i] = dirs[i].split('|')[0]
-    real_dirs = utils.get_dirs(args.path)
-    if len(dirs) == len(real_dirs):
-        print(','.join(dirs), end='')
-    else:
-        utils.log('compare', 'error')
-        utils.log('file_dirs', ','.join(real_dirs), 'error')
-        utils.log('statistical', ','.join(dirs), 'error')
+        if len(dirs) == len(real_dirs):
+            print(','.join(dirs), end='')
+        else:
+            utils.log('compare', 'error')
+            utils.log('file_dirs', ','.join(sorted(real_dirs)), 'error')
+            utils.log('statistical', ','.join(sorted(dirs)), 'error')
+
     # print(','.join(dirs), end='')
