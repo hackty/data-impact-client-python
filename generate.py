83334e580cdbfaaefcf074c36e447a96d6cf32db
Evan <yjq877978471@gmail.com>
2018-10-11 11:00:53 +0800

    fix

--- generate.py
+++ generate.py
@@ -21,7 +21,7 @@ def get_data(conn, sql):
 
 
 # 生成来自db的数据包
-def handle_db(args, name):
+def handle_db(args, name, now):
     conn = utils.get_mysql_conn(args.host, args.dbUser, args.dbPassword, args.database)
     cursor = get_data(conn, args.sql)
     meta = {}
@@ -52,19 +52,24 @@ def handle_db(args, name):
 
 
 # 生成来自文件的数据包
-def handle_file(args, name):
+def handle_file(args, name, now):
     meta = {}
     count = 0
+    col_size = len(args.columnName.split(','))
     with open(args.sourceFile, 'r', encoding='utf-8') as rf:
         for line in rf:
             with open(name, 'a') as wf:
-                wf.write('|||'.join(line.strip().split(args.separator)).replace('\n', '') + '\n')
                 count = count + 1
+                rows = line.strip().split(args.separator)
+                if len(rows) != col_size:
+                    utils.log('err_source', now+'/'+str(count), 'error')
+                    return {}
+                wf.write('|||'.join(rows).replace('\n', '') + '\n')
     with open(name, 'r', encoding='utf-8') as f:
         content = f.read()
     meta.__setitem__("size", count)
     meta.__setitem__("md5", utils.get_md5(content))
-    meta.__setitem__("colName", ','.join(args.columnName))
+    meta.__setitem__("colName", args.columnName)
     return meta
 
 
@@ -89,15 +94,17 @@ def run(args):
     real_path = path + "/" + name + ".data"
 
     # 生成数据包文件
-    meta = sourceFilter()[args.sourceType](args, real_path)
+    meta = sourceFilter()[args.sourceType](args, real_path, now)
 
     # 生成meta文件
-    real_path = path + "/" + name + ".meta"
-    meta.__setitem__("timestamp", now)
-    meta.__setitem__("dataName", real_path)
-    meta.__setitem__("tagName", args.tagName)
-    meta_json = utils.to_json(meta)
-    generate_meta(real_path, utils.encode(meta_json))
-
-    utils.edit_list(now, utils.lan('generated') + '\n')
-    utils.log('generated', now)
+    if meta != {} :
+        real_path = path + "/" + name + ".meta"
+        meta.__setitem__("timestamp", now)
+        meta.__setitem__("dataName", real_path)
+        meta.__setitem__("tagName", args.tagName)
+        meta_json = utils.to_json(meta)
+        generate_meta(real_path, utils.encode(meta_json))
+        utils.edit_list(now, utils.lan('generated') + '\n')
+        utils.log('generated', now)
+    else :
+        utils.log('err_generate', now)
